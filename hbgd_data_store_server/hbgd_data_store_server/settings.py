55bcb75c54a35f01bdaae08f353f1cab4e9efcbc
Patrick <pcstout@gmail.com>
2020-01-08 08:57:00 -0700

    Get database user from ENV

--- hbgd_data_store_server/hbgd_data_store_server/settings.py
+++ hbgd_data_store_server/hbgd_data_store_server/settings.py
@@ -25,6 +25,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import getpass
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -148,8 +149,8 @@ else:
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': 'hbgd',
-            'USER': 'hbgd',
-            'PASSWORD': os.environ.get('DB_PASSWORD'),
+            'USER': os.environ.get('DB_USERNAME', getpass.getuser()),
+            'PASSWORD': os.environ.get('DB_PASSWORD', None),
             'HOST': 'localhost',
             'PORT': '5432',
         }
